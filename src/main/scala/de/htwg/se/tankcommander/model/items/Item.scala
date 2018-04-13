f5af54197a1bcdb7ab7e20b9df1e3b223aaeadd2
Robin Weisshaar <37539052+RoWe90@users.noreply.github.com>
2018-04-13 06:57:45 -0400

    working on traits

--- src/main/scala/de/htwg/se/tankcommander/model/items/Item.scala
+++ src/main/scala/de/htwg/se/tankcommander/model/items/Item.scala
@@ -4,7 +4,15 @@ package de.htwg.se.tankcommander.model.items
 trait Item {
    val itemName : String
    val description : String
+  //time for the duration of the item, as soon as it's activated
+  val duration : Integer
 
-
-
+//activate Item, apply buffs, return duration
+  def activate(item: Item):Integer ={
+    duration
+  }
+  //Remove Buff from player, return true when successful
+  def deactivate(item: Item):Boolean={
+    false
+  }
 }
