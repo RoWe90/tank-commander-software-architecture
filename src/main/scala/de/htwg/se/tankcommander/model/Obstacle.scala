33686e5be7ddc69b3cac01ea6920c86fbc80569b
RoWe90 <robin.weisshaar@gmail.com>
2018-06-12 16:35:37 +0200

    changed move methods changed utf symbols for game-grid

--- src/main/scala/de/htwg/se/tankcommander/model/Obstacle.scala
+++ src/main/scala/de/htwg/se/tankcommander/model/Obstacle.scala
@@ -12,35 +12,35 @@ class Bush extends Obstacle {
   val name: String = "Gebüsche"
   val desc: String = "Gebüsche verringern die Präzision von Schüssen"
   val passable: Boolean = true
-  val shortName: String = "B"
+  val shortName: String = "\u16E4"
 }
 
 class Hill extends Obstacle {
   val name: String = "Hügel"
   val desc: String = "Ermöglichen direkten Beschuss mapweit"
   val passable: Boolean = false
-  val shortName: String = "H"
+  val shortName: String = "\u0245"
 }
 
 class Rock extends Obstacle {
   val name: String = "Stein"
   val desc: String = "Steine dienen als Hinderniss und können weder passiert noch durschossen werden"
   val passable: Boolean = false
-  val shortName: String = "S"
+  val shortName: String = "\u26C6"
 }
 
 class Forest extends Obstacle {
   val name: String = "Wald"
   val desc: String = "Wälder liefern Schutz und verringern die Hitchance des Gegners"
   val passable: Boolean = true
-  val shortName: String = "F"
+  val shortName: String = "\u00A5"
 }
 
 class Water extends Obstacle {
   val name: String = "Wasser"
   val desc: String = "Wasser kann nicht passiert werden"
   val passable: Boolean = false
-  val shortName: String = "W"
+  val shortName: String = "\u2635"
 }
 
 
