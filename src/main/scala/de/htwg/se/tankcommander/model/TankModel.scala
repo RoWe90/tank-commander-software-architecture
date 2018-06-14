eff3bdd49f71518594c085cf03e74afb8e7ba636
Sebastian <37550703+seliehr@users.noreply.github.com>
2018-06-14 18:42:06 +0200

    Generic

--- src/main/scala/de/htwg/se/tankcommander/model/TankModel.scala
+++ src/main/scala/de/htwg/se/tankcommander/model/TankModel.scala
@@ -1,13 +1,10 @@
 package de.htwg.se.tankcommander.model
 
-
-//TODO Define a class containing all needed values and methods for a tank-object
 class TankModel() {
   val tankBaseDamage = 10
   val accuracy = 100
   var healthpoints = 100
-  var posC:Option[Cell] = None
+  var posC: Option[Cell] = None
   var facing = "up"
-
 }
 
