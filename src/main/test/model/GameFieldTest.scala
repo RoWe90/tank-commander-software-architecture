296e8379648cfe0a9866575ba8802546b3aec062
RoWe90 <37539052+RoWe90@users.noreply.github.com>
2018-06-19 19:37:34 +0200

    updated Tests for: -GameField -TankModel

--- src/main/test/model/GameFieldTest.scala
+++ src/main/test/model/GameFieldTest.scala
@@ -4,14 +4,13 @@ import de.htwg.se.tankcommander.model._
 import org.scalatest.{FlatSpec, Matchers}
 
 class GameFieldTest extends FlatSpec with Matchers {
-  //@TODO: How to test private Methods?!
   "A GameField Object" should "have an Array with a size of 11x11" in {
     val gameField = new GameField
     assert(gameField.gridsX === 11 & gameField.gridsy === 11)
 
 
   }
-  it should "fill a GameField with Cells" in {
+  it should "be filled with Cells" in {
 
     val gameField = new GameField
 
@@ -33,11 +32,44 @@ class GameFieldTest extends FlatSpec with Matchers {
 
     for (y <- 0 until gameField.gridsX) {
       for (x <- 0 until gameField.gridsy) {
-        assert(gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Bush]] === true | gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Hill]] === true |
-          gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Rock]] === true | gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Forest]] === true |
+        assert(gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Bush]] === true |
+          gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Hill]] === true |
+          gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Rock]] === true |
+          gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Forest]] === true |
           gameField.marray(x)(y).cobstacle.isInstanceOf[Option[Water]])
       }
     }
+  }
+  "A GameField Object" should "print out the Gamefield with toString" in {
+    val gameField = new GameField
+    for (y <- 0 until gameField.gridsX) {
+      for (x <- 0 until gameField.gridsy) {
+        gameField.marray(x)(y).cobstacle = Option(new Bush)
+      }
+    }
+    val tank1 = new TankModel
+    val tank2 = new TankModel
+    gameField.marray(0)(0).containsThisTank = Option(tank1)
+    gameField.marray(10)(10) = new Cell(Position(10, 10))
+    gameField.marray(10)(10).containsThisTank = Option(tank2)
+    gameField.marray(9)(10) = new Cell(Position(9, 10))
+
+
+    assert(gameField.toString === "\n" +
+      "T  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  B  B  \n" +
+      "B  B  B  B  B  B  B  B  B  o  T  HS: 0\n"
+    )
+
+
   }
 
 }
